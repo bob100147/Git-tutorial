24ce9ec161777b705a8b0a81e4a57759000d6832
bob100147 <schwab@econ.umd.edu>
2019-02-27 14:52:30 -0800

    Added second print statement.

--- Git tutorial/ViewController.swift
+++ Git tutorial/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        print("Hello Stanford computer class!")
         print("Hello world!")
         super.viewDidLoad()
     }
